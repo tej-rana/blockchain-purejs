2dfbe9ff9ec874db4adb41bf65030d63c90031da
Tej Rana <tej@allstrata.tech>
2021-06-03 16:50:33 +1000

    Added peer to peer mining, proof of work and difficulty adjustment

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../src/blockchain');
+const P2pserver = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pserver(bc);
 
 app.use(bodyParser.json());
 
@@ -17,8 +19,11 @@ app.post('/mine', (req, res) => {
    const block = bc.addBlock(req.body.data);
    console.log(`New block added ${block.toString()}`);
    res.redirect('/blocks');
+   p2pServer.syncChains();
 });
 
 app.listen(HTTP_PORT, () => {
    console.log(`listening on port ${HTTP_PORT}`);
 });
+
+p2pServer.listen();
--- test/block.test.js
+++ test/block.test.js
@@ -15,4 +15,19 @@ describe('Block', () => {
     it('sets the `lasthHash` to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
     });
+
+    it('generates a hash that matches the difficulty', () => {
+        console.log('Block is: ', block);
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
+
+    it('it lowers difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 3600000))
+            .toEqual(block.difficulty - 1);
+    });
+
+    it('it raises difficulty for quickly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 1))
+            .toEqual(block.difficulty + 1);
+    });
 });

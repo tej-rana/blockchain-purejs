e2fa6c9a5ca10efbcb49280942046848a88ebcca
Tej Rana <tej@allstrata.tech>
2021-06-05 09:52:57 +1000

    Finished create wallet function

--- test/index.js
+++ test/index.js
@@ -1,8 +1,13 @@
-const Blockchain = require('../src/blockchain/blockchain');
+/*const Blockchain = require('../src/blockchain/blockchain');
 
 const bc = new Blockchain();
 
 for (let i=0; i<10; i++) {
     console.log(bc.addBlock(`for ${i}`).toString());
 }
+*/
 
+const Wallet = require('../src/wallet');
+const wallet = new Wallet();
+
+console.log(wallet.toString());
